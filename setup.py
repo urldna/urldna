f2bc519024a40b82e5a33616554f560f62621da8
Paolo Ripamonti <paolo.ripamonti93@gmail.com>
2024-05-27 11:13:25 +0200

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 VERSION = '0.0.1' 
-DESCRIPTION = 'urlDNA python package'
-LONG_DESCRIPTION = 'urlDNA is a powerful tool for comprehensive URL analysis, advanced brand monitoring, phishing detection, and custom query capabilities. This Python package allows users to interact with the urlDNA API seamlessly through Python.'
+DESCRIPTION = 'urlDNA is a powerful tool for comprehensive URL analysis, advanced brand monitoring, phishing detection, and custom query capabilities. This Python package allows users to interact with the urlDNA API seamlessly through Python.'
 
 setup(
     name="urlDNA", 
     version=VERSION,
     author="urlDNA",
-    license="MIT License",
+    license="MIT",
     url="https://urldna.io",
     author_email="urldna@urldna.io",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=["requests", "marshmallow"],
     keywords=['python', 'web scraping', 'website analysis', 'url scan'],
     classifiers= [
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Education",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ]
 )
