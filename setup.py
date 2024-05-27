47c7597c7d9ef603cac6502d327cad38f6539aa2
Paolo Ripamonti <paolo.ripamonti93@gmail.com>
2024-05-27 11:06:08 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,26 +4,24 @@ VERSION = '0.0.1'
 DESCRIPTION = 'urlDNA python package'
 LONG_DESCRIPTION = 'urlDNA is a powerful tool for comprehensive URL analysis, advanced brand monitoring, phishing detection, and custom query capabilities. This Python package allows users to interact with the urlDNA API seamlessly through Python.'
 
-# Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="urlDNA", 
-        version=VERSION,
-        author="urlDNA",
-        license="MIT License",
-        url="https://urldna.io",
-        author_email="urldna@urldna.io",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=["requests", "marshmallow"],
-        keywords=['python', 'web scraping', 'website analysis', 'url scan'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ]
+    name="urlDNA", 
+    version=VERSION,
+    author="urlDNA",
+    license="MIT License",
+    url="https://urldna.io",
+    author_email="urldna@urldna.io",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["requests", "marshmallow"],
+    keywords=['python', 'web scraping', 'website analysis', 'url scan'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
