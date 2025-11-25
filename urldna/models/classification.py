49cb9dddb40eb5a53763c62c266b7231311dd8f8
urldna <paolo.ripamonti93@gmail.com>
2025-11-25 19:24:49 +0100

    Bug fix classification verdict

--- urldna/models/classification.py
+++ urldna/models/classification.py
@@ -1,12 +1,9 @@
 class Classification(object):
 
-    def __init__(self,
-                verdict=None,
-                threat=None) -> None:
+    def __init__(self, verdict=None) -> None:
         
         # Fields
         self.verdict = verdict
-        self.threat = threat
 
     def __repr__(self):
         return "<Classification verdict: %r >" % self.verdict
--- urldna/schemas/classification_schema.py
+++ urldna/schemas/classification_schema.py
@@ -2,11 +2,10 @@
 from marshmallow import Schema, fields, post_load
 
 # Models
-from urldna.models.classifcation import Classification
+from urldna.models.classification import Classification
 
 class ClassificationSchema(Schema):
     verdict = fields.String(allow_none=True)
-    threat = fields.String(allow_none=True)
 
     @post_load
     def create(self, data, **kwargs):
